d7f4eabbf2d9fc4ad9ce45ba3bb92b215c19a030
GabrielD <gdedi651@gmail.com>
2016-01-08 20:33:33 -0500

    add animation function to handle image

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 (function() {
+	
+	/* animation function */
+	function animationClick(element, animation) {
+		element = $(element);
+		(element).on('click', function(){
+			element.addClass('animated ' + animation);
+			// wait for animation
+			window.setTimeout( function(){
+				element.removeClass('animated ' + animation);
+			}, 2000);
+		});
+	}
+	
 	$(document).ready(function() {
-		//alert("hello world");
+		
+		
+	/*	 //alert("hello world");
 		$('i').on('click', function(){
 			var mag = $(this).find('#magnify');
 			// mag.animate({'font-size': '+=4000px'}, 'slow');
@@ -13,9 +28,10 @@
 				'margin-top': '-=30px',
 				'margin-left': '-=30px'
 			}, 'slow');
-			/*mag.animate({
+			mag.animate({
 				left: '30px'
-			});*/
+			});
 		});
+	*/
 	});
 })();

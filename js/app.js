a170fc4e3e8d60fe37cb9dd9d8f9c5a35d7d542f
GabrielD <gdedi651@gmail.com>
2016-01-09 23:13:31 -0500

    organize code based on new chosen animation

--- js/app.js
+++ js/app.js
@@ -1,37 +1,20 @@
 (function() {
 	
 	/* animation function */
-	function animationClick(element, animation) {
+	function animationClick(element, animation1, animation2) {		
 		element = $(element);
 		(element).on('click', function(){
-			element.addClass('animated ' + animation);
-			// wait for animation
+			element.addClass('animated ' + animation1);
+			// wait for animation 1
 			window.setTimeout( function(){
-				element.removeClass('animated ' + animation);
-			}, 2000);
+				element.removeClass('animated ' + animation1);
+				element.addClass('animated ' + animation2);
+				$('#label').fadeOut();
+			}, 830);
 		});
 	}
 	
 	$(document).ready(function() {
-		
-		
-	/*	 //alert("hello world");
-		$('i').on('click', function(){
-			var mag = $(this).find('#magnify');
-			// mag.animate({'font-size': '+=4000px'}, 'slow');
-			// mag.animate({'margin-top': '-=300'}, 'slow');
-			$(this).closest('#magnify').animate({
-				top: '-980px',
-			}, 'slow');
-			$(this).animate({
-				'font-size': '+=4000px',
-				'margin-top': '-=30px',
-				'margin-left': '-=30px'
-			}, 'slow');
-			mag.animate({
-				left: '30px'
-			});
-		});
-	*/
+		animationClick('#magnify', 'pulse', 'zoomOutUp');
 	});
 })();
